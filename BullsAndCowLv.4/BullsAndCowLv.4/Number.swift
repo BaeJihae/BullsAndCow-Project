34f9c2f327128f736c75d1db060b6ca93bed5561
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-14 17:42:06 +0900

    number 클래스 생성해 Lv.3에서 긁어오기

--- BullsAndCowLv.4/BullsAndCowLv.4/Number.swift
+++ BullsAndCowLv.4/BullsAndCowLv.4/Number.swift
@@ -5,4 +5,22 @@
 //  Created by 배지해 on 3/14/24.
 //
 
-import Foundation
+class Number {
+    
+    var num: Int?
+    
+    func getRandomNum() {
+        var num = [Int]()
+        
+        while num.count < 3 {
+            let a = Int.random(in: 0...9)
+            if num == [] && a != 0 {
+                num.append(a)
+            }else if num != [] && !num.contains(a) {
+                num.append(a)
+            }
+        }
+        
+        self.num = num[0] * 100 + num[1] * 10 + num[2] * 1
+    }
+}
