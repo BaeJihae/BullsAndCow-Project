4678a070f6d93870ca4828a6dce16b3e73705c51
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-14 17:27:55 +0900

    Lv.1과 Lv.2의 Number 클래스 긁어오기

--- BullsAndCowLv.3/BullsAndCowLv.3/Number.swift
+++ BullsAndCowLv.3/BullsAndCowLv.3/Number.swift
@@ -5,4 +5,20 @@
 //  Created by 배지해 on 3/14/24.
 //
 
-import Foundation
+class Number {
+    
+    var num: Int?
+    
+    func getRandomNum() {
+        var num = [Int]()
+        
+        while num.count < 3 {
+            let a = Int.random(in: 0...9)
+            if ( num == [] && a != 0 ) || !num.contains(a) {
+                num.append(a)
+            }
+        }
+        
+        self.num = num[0] * 100 + num[1] * 10 + num[2] * 1
+    }
+}
