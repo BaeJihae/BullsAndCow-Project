a74c700f5614cad0a09c66a46c05df6f01c18600
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-14 15:06:30 +0900

    유저에게 입력값을 받는 함수를 생성 입력값은 1부터 9까지의 3자리 정수값을 가져야 하므로 에러처리를 해줌.

--- BullsAndCow/BullsAndCow/main.swift
+++ BullsAndCow/BullsAndCow/main.swift
@@ -11,11 +11,46 @@
 // readLine() 함수를 이용하여 유저의 입력값 처리하기
 // readLine() 함수에 대해 학습해보고 활용하기
 
-let answer = Number()
 
+// 유저에게 입력값을 받는 함수
+func getUserInput() -> Int {
+    let input = readLine()
+    
+    // 문자열이 입력되지 않았을 경우 에러처리
+    guard let input = input else { return getUserInput() }
+    
+    // 문자열이 3자리가 아닐 경우 에러처리
+    if input.count != 3 {
+        print("1부터 9까지를 자릿수로 가지는 3자리 숫자를 입력해주세요.")
+        return getUserInput()
+    }
+    
+    // 문자열에 0이 포함되어 있는 경우 에러처리
+    if input.contains("0") {
+        print("1부터 9까지의 숫자를 입력해주세요.")
+        return getUserInput()
+    }
+    
+    // 문자열이 숫자가 아닐 경우 에러 처리
+    guard let input = Int(input) else {
+        print("숫자를 입력해주세요.")
+        return getUserInput()
+    }
+    
+    return input
+}
 
-
-let input = readLine()
-
+// 게임 시작 함수
+func startGame() {
+    
+    print("게임이 시작되었습니다.")
+    print("===========================")
+    
+    let number = Number()
+    number.getRandomNum()
+    
+    print("랜덤한 세자리 정수가 설정되었습니다.")
+    
+}
 
 
