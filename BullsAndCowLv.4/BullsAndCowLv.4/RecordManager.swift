123b62f5ee63d3b0210909aad799897d5196ec0d
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-14 20:54:20 +0900

    RecordManager의 세부 구현 - 유저들의 게임 기록을 기록할 배열 - 유저들의 게임 정보를 저장할 함수 - 유저들의 게임 정보를 출력하는 함수

--- BullsAndCowLv.4/BullsAndCowLv.4/RecordManager.swift
+++ BullsAndCowLv.4/BullsAndCowLv.4/RecordManager.swift
@@ -5,4 +5,20 @@
 //  Created by 배지해 on 3/14/24.
 //
 
-import Foundation
+class RecordManager {
+    
+    // 유저들의 게임 기록을 기록할 배열
+    private var recordArray: [Int] = []
+    
+    // 유저들의 게임 정보를 저장할 함수
+    func setRecordArray(_ count: Int) {
+        recordArray.append(count)
+    }
+    
+    // 유저들의 게임 정보를 출력하는 함수
+    func getRecordArray() {
+        recordArray.enumerated().forEach{ (i, count) in
+            print("\(i+1)번째 게임 : 시도 횟수 \(count)")
+        }
+    }
+}
