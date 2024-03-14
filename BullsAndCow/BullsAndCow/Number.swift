16fb7d81af89137b0d1b2030f010ffd645fbe6e3
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-14 14:35:32 +0900

    랜덤한 숫자를 출력하는 randomNum 함수 이름을 좀 더 직관적으로 getRandomNum으로 변경

--- BullsAndCow/BullsAndCow/Number.swift
+++ BullsAndCow/BullsAndCow/Number.swift
@@ -4,5 +4,12 @@
 //
 //  Created by 배지해 on 3/14/24.
 //
-
-import Foundation
+class Number {
+    
+    var num: Int?
+    
+    func getRandomNum() -> Int {
+        return (1...3).map{ $0*$0*Int.random(in: 1...9) }.reduce(0, +)
+    }
+    
+}
--- BullsAndCow/BullsAndCow/main.swift
+++ BullsAndCow/BullsAndCow/main.swift
@@ -4,8 +4,13 @@
 //
 //  Created by 배지해 on 3/14/24.
 //
+// - Lv1
+// - [ ]  1에서 9까지의 서로 다른 임의의 수 3개를 정하고 맞추는 게임입니다
+// - [ ]  정답은 랜덤으로 만듭니다.(1에서 9까지의 서로 다른 임의의 수 3자리)
 
-import Foundation
+// readLine() 함수를 이용하여 유저의 입력값 처리하기
+// readLine() 함수에 대해 학습해보고 활용하기
 
-print("Hello, World!")
+let input = readLine()
+
 
