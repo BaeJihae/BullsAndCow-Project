052feac2311d6c5df8667ded946083174dc91bdf
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-14 17:26:40 +0900

    Lv.1과 2의 User 함수 긁어오기

--- BullsAndCowLv.3/BullsAndCowLv.3/User.swift
+++ BullsAndCowLv.3/BullsAndCowLv.3/User.swift
@@ -5,4 +5,22 @@
 //  Created by 배지해 on 3/14/24.
 //
 
-import Foundation
+class User {
+    
+    // 정답까지의 횟수
+    var count: Int = 0
+    
+    // 유저가 제시한 숫자
+    var userNum: Int?
+    
+    init() {
+        self.count = 0
+        self.userNum = nil
+    }
+    
+    init(count: Int, userNum: Int? = nil) {
+        self.count = count
+        self.userNum = userNum
+    }
+    
+}
